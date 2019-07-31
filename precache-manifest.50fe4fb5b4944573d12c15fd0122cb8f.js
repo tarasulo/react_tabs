3581bc21caf917ce09fe49dde9005cc03a8475ac
Taras Khalak <tarasulo@gmail.com>
2019-07-31 09:52:17 +0300

    Updates

---
+++ precache-manifest.50fe4fb5b4944573d12c15fd0122cb8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea32d1699d8ef7bad750ff1d9591f9cb",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "0f30db49b4ca192f69a9",
+    "url": "/react_tabs/static/css/main.0f65a27c.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react_tabs/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "0f30db49b4ca192f69a9",
+    "url": "/react_tabs/static/js/main.794bb2b3.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
